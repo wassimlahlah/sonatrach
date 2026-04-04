d04d712d0a24293c06340191e759f0ae010b36b8
imadboul <didouredjas2020@gmail.com>
2026-04-04 23:13:52 +0100

    imad1

--- backend/projectPFE-backend-main/user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import signup, login, verifyEmail, refresh_access
-
-urlpatterns = [
-    path('signUp/', signup),
-    path('verifyEmail/<str:token>/', verifyEmail),
-    path('login/', login),
-    path('refresh/', refresh_access),
-]
